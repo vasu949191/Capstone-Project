8444c62d0cc28c62a6b1ca037a727124f93abbfa
vasu949191 <66633385+vasu949191@users.noreply.github.com>
2020-06-08 10:26:18 -0600

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # First Assignemnt
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
